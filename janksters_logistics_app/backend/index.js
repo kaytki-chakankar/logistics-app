7157e6e42a2e08e5ed0c3cd89d2fd62741aa6c16
kaytki-chak <149740324+kaytki-chak@users.noreply.github.com>
2025-09-08 01:09:36 -0700

    9/4 attendance

--- janksters_logistics_app/backend/index.js
+++ janksters_logistics_app/backend/index.js
@@ -293,7 +293,7 @@ app.get('/attendance/:email', async (req, res) => {
     console.log(meetings)
 
     // total meeting hours
-    let totalMeetingHours = 12;
+    let totalMeetingHours = 17;
     if (isRookie) totalMeetingHours -= 2.5;
 
     // total hours attended

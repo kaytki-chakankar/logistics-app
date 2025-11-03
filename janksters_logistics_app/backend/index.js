e4aff97cd2bd5c0f4ddd87cde60414caabe77d4d
kaytki-chak <149740324+kaytki-chak@users.noreply.github.com>
2025-11-03 02:24:08 -0800

    path fix 4 :)

--- janksters_logistics_app/backend/index.js
+++ janksters_logistics_app/backend/index.js
@@ -11,7 +11,7 @@ const PORT = process.env.PORT || 3000;
 app.use(cors());
 
 const auth = new google.auth.GoogleAuth({
-  keyFile: '/var/run/secrets/service-account.json', // path Render gives
+  keyFile: "/etc/secrets/service-account.json",
   scopes: ['https://www.googleapis.com/auth/spreadsheets'],
 });
 

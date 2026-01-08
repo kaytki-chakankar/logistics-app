249f0268961e1a320c5794545519e10e5ba8c5a6
kaytki-chak <149740324+kaytki-chak@users.noreply.github.com>
2026-01-08 06:39:49 -0800

    flutter build

--- janksters_logistics_app/backend/index.js
+++ janksters_logistics_app/backend/index.js
@@ -10,6 +10,7 @@ const PORT = process.env.PORT || 3000;
 
 app.use(cors());
 app.use(express.json());
+app.use(express.static(path.join(__dirname, 'public'))); 
 
 const auth = new google.auth.GoogleAuth({
   keyFile: "/etc/secrets/service_account.json",
